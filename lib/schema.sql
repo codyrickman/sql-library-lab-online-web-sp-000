d1fa660d45bf2b88d9dfb6252074f49d0e2bd1b9
Cody Rickman <cody@creativemindstudios.com>
2019-09-05 00:56:07 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,10 +2,15 @@ CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title STRING,
   author_id INTEGER,
-  sub-genre_id INTEGER
+  subgenre_id INTEGER
 )
 
-CREATE TABLE genre(
+CREATE TABLE subgenres(
+  id INTEGER PRIMARY KEY,
+  name STRING
+)
+
+CREATE TABLE authors(
   id INTEGER PRIMARY KEY,
   name STRING
 )
