059f5001ca997dd493d52b19d9bec89c41d23326
Cody Rickman <cody@creativemindstudios.com>
2019-09-05 02:00:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM character_books INNER JOIN books ON character_books.book_id = books.id INNER JOIN series ON books.series_id = series.id INNER JOIN characters ON character_books.character_id = characters.id"
+  "SELECT series.title FROM character_books INNER JOIN books ON character_books.book_id = books.id INNER JOIN series ON books.series_id = series.id INNER JOIN characters ON character_books.character_id = characters.id WHERE characters.species = 'human'"
 end
 
 def select_character_names_and_number_of_books_they_are_in
